8895d1ed5127df8d0c5c9e0dc0c8d852912c6e7e
cms5380 <cms1297@gmail.com>
2021-02-28 01:53:28 +0900

    sort

---
+++ baekjoon/#5052.py
@@ -0,0 +1,25 @@
+import sys
+
+# T = int(sys.stdin.readline())
+
+# for _ in range(T):
+#     n = int(sys.stdin.readline())
+#     nums = [sys.stdin.readline().strip() for _ in range(n)]
+
+#     nums.sort()
+#     ans = False
+#     for i in range(len(nums)-1):
+#         if nums[i] in nums[i+1]:
+#             ans = True
+#             break
+
+#     if ans:
+#         print("NO")
+#     else:
+#         print("YES")
+
+print("12")
+print("12")
+print("12")
+print("12")
+print("12")
